1d57061984d1e09976bb395402368f2dc811c653
Park JaeHyeong <106136905+jehyngn@users.noreply.github.com>
2023-08-17 18:30:27 +0200

    Update battery_sender.py

--- src/sender/battery_sender.py
+++ src/sender/battery_sender.py
@@ -4,9 +4,11 @@ import dbus.service
 from piracer.vehicles import PiRacerStandard
 from collections import deque
 
+import time
+
 def get_battery():
     voltage = piracer.get_battery_voltage() / 3
-    print(voltage)
+    # print(voltage)
     if(voltage>4.2):
         battery_percentage = 100
     elif(voltage>=4.1):
@@ -20,7 +22,7 @@ def get_battery():
     elif(voltage>=3.6):
         battery_percentage = 0 + ((voltage-3.6)/(3.8-3.6))*(30-0)
     else:
-        return voltage        
+        battery_percentage = 0  
        
         
     return battery_percentage
@@ -35,8 +37,12 @@ if __name__ == "__main__":
 
     while 1:
         list.popleft()
+        time_1 = time.time()
         now_battery = get_battery() 
         list.append(now_battery)
         battery = max(list)
         # print(float(battery))
         car_interface.setBattery(float(battery))
+        time.sleep(0.01)
+        time_2 = time.time()
+        print("time: ", time_2 - time_1)
